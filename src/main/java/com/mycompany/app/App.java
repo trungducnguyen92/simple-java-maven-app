0240371c7cf070549401eb9de093a8148a36d25f
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2020-12-30 21:25:09 +0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World test webhook lan 5!";
+    private final String message = "Hello World test webhook lan 6!";
 
     public App() {}
 
